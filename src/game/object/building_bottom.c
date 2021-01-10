22eb867c2007acad38593a3db32e6d84f0711d1b
Gabriel Ravier <gabravier@gmail.com>
2021-01-10 15:00:03 +0100

    Fixed norm in game/object/building_bottom
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- src/game/object/building_bottom.c
+++ src/game/object/building_bottom.c
@@ -69,20 +69,12 @@ static sfRectangleShape *make_line_below(const sfRectangleShape *line, sfUint8 r
     return (result);
 }
 
-struct game_object game_object_create_building_bottom(
+static void make_lines(struct building_bottom_data *data,
     struct game_state_play_sequence *sequence)
 {
-    struct game_object result;
-    struct building_bottom_data *data = malloc(sizeof(*data));
-
-    MY_ASSERT(data != NULL);
-    result.draw = do_draw;
-    result.destroy = do_destroy;
-    result.update = NULL;
     data->top_line = sfRectangleShape_create();
     sfRectangleShape_setPosition(data->top_line, sequence->position);
-    sfRectangleShape_setSize(data->top_line,
-        (sfVector2f){sequence->width, 1});
+    sfRectangleShape_setSize(data->top_line, (sfVector2f){sequence->width, 1});
     sfRectangleShape_setFillColor(data->top_line, sfColor_fromRGB(77, 77, 89));
     data->line2 = make_line_below(data->top_line, 255, 255, 255);
     data->line3 = make_line_below(data->line2, 100, 106, 125);
@@ -96,6 +88,19 @@ struct game_object game_object_create_building_bottom(
     data->rect_other_lines = make_line_below(data->line89, 176, 176, 191);
     sfRectangleShape_setSize(data->rect_other_lines, (sfVector2f){
         sfRectangleShape_getSize(data->rect_other_lines).x, 10000});
+}
+
+struct game_object game_object_create_building_bottom(
+    struct game_state_play_sequence *sequence)
+{
+    struct game_object result;
+    struct building_bottom_data *data = malloc(sizeof(*data));
+
+    MY_ASSERT(data != NULL);
+    result.draw = do_draw;
+    result.destroy = do_destroy;
+    result.update = NULL;
+    make_lines(data, sequence);
     result.rect = (sfFloatRect){sequence->position.x, sequence->position.y,
         sequence->width, sequence->height};
     result.private_data = data;
